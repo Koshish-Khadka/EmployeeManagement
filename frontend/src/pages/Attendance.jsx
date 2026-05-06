c6cc1a9f738cbd5c5f82b97461e5eb321ea1b9c1
koshish khadka <koshishkhadka364@gmail.com>
2026-05-06 11:32:44 +0545

    attendance system

--- frontend/src/pages/Attendance.jsx
+++ frontend/src/pages/Attendance.jsx
@@ -1,7 +1,59 @@
+import {
+  QueryClient,
+  useMutation,
+  useQuery,
+  useQueryClient,
+} from "@tanstack/react-query";
 import { DoorOpen, FilePenLine, LogOutIcon } from "lucide-react";
 import React from "react";
+import api from "../axios/axios";
+import toast from "react-hot-toast";
 
 const Attendance = () => {
+  const queryClient = useQueryClient();
+
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["attendance"],
+    queryFn: async () => {
+      const response = await api.get("/attendance");
+      return response.data;
+    },
+  });
+
+  const handleAttendance = useMutation({
+    mutationFn: async () => {
+      const response = await api.post("/attendance");
+      return response.data;
+    },
+    onSuccess: () => {
+      toast.success("Check-In Success");
+      queryClient.invalidateQueries({ queryKey: ["attendance"] });
+    },
+    onError: () => {
+      toast.error("Failed to check-in");
+    },
+  });
+  const formatLocalDate = (date) => new Date(date).toLocaleDateString("en-CA");
+
+  const today = formatLocalDate(new Date());
+
+  const todayAttendance = data?.data?.find(
+    (item) => formatLocalDate(item.date) === today,
+  );
+
+  const isCheckedIn = todayAttendance && !todayAttendance.check_out;
+  const isCheckedOut = todayAttendance && todayAttendance.check_out;
+  
+  // const today = new Date().toLocaleDateString("en-CA");
+  // const todayAttendance = data?.data?.find(
+  //   (item) => new Date(item.date).toLocaleDateString("en-CA") === today,
+  // );
+
+  // console.log("Today attendance", todayAttendance);
+
+  // const isCheckedIn = todayAttendance && !todayAttendance.check_out;
+  // const isCheckedOut = todayAttendance && todayAttendance.check_out;
+
   return (
     <div className="relative">
       <div>
@@ -42,14 +94,37 @@ const Attendance = () => {
       </div>
       {/* Attendance Button */}
       <div className="fixed bottom-5 right-5 transition-all duration-200 hover:scale-105">
-        <button className="border flex justify-between items-center gap-x-3 px-3 py-4 rounded-lg bg-green-700 text-white">
+        <button
+          className={`border flex items-center gap-x-3 px-3 py-4 rounded-lg text-white ${
+            isCheckedOut
+              ? "bg-gray-500 cursor-not-allowed"
+              : isCheckedIn
+                ? "bg-slate-700"
+                : "bg-green-700"
+          }`}
+          onClick={() => handleAttendance.mutate()}
+          disabled={isCheckedOut}
+        >
           <DoorOpen className="h-8 w-8" />
           <div>
-            <p>Clock In</p>
-            <p className="text-[10px] text-gray-100 ">start your work day</p>
+            <p>
+              {isCheckedOut
+                ? "Completed"
+                : isCheckedIn
+                  ? "Clock Out"
+                  : "Clock In"}
+            </p>
+            <p className="text-[10px] text-gray-100">
+              {isCheckedOut
+                ? "You have completed today"
+                : isCheckedIn
+                  ? "End your work day"
+                  : "Start your work day"}
+            </p>
           </div>
         </button>
       </div>
+
       {/* table */}
       <div className="card overflow-hidden mt-8">
         <div className="overflow-x-hidden">
@@ -65,11 +140,46 @@ const Attendance = () => {
               </tr>
             </thead>
             <tbody>
-              <tr>
-                <td colSpan="4" className="text-center py-12 text-slate-400">
-                  No records found
-                </td>
-              </tr>
+              {data?.data.length === 0 ? (
+                <tr>
+                  <td colSpan="4" className="text-center py-12 text-slate-400">
+                    No records found
+                  </td>
+                </tr>
+              ) : isLoading ? (
+                <tr>
+                  <td colSpan="4" className="text-center py-12 text-slate-400">
+                    Content Loading
+                  </td>
+                </tr>
+              ) : isError ? (
+                <tr>
+                  <td colSpan="4" className="text-center py-12 text-slate-400">
+                    Failed to Load Content{" "}
+                  </td>
+                </tr>
+              ) : (
+                data?.data?.map((data) => (
+                  <tr key={data.id}>
+                    <td> {new Date(data.date).toLocaleDateString()}</td>
+
+                    <td>
+                      {data.check_in
+                        ? new Date(data.check_in).toLocaleTimeString()
+                        : "-"}
+                    </td>
+                    <td>
+                      {data.check_out
+                        ? new Date(data.check_out).toLocaleTimeString()
+                        : "-"}
+                    </td>
+
+                    <td>{data.workinghour}</td>
+                    <td>{data.daytype}</td>
+                    <td>{data.status}</td>
+                  </tr>
+                ))
+              )}
             </tbody>
           </table>
         </div>
--- server/controllers/attendanceController.js
+++ server/controllers/attendanceController.js
@@ -5,22 +5,89 @@ import pool from "../config/db.js";
 export const checkInOut = async (req, res) => {
   try {
     const userId = req.session.userId;
-    const employee = await pool.query(
+    const employeeResult = await pool.query(
       `SELECT * FROM employees WHERE userId=$1 LIMIT 1`,
       [userId],
     );
-    if (employee.rows.length === 0) {
+
+    if (employeeResult.rows.length === 0) {
       return res.status(404).json({ error: "Employee not found" });
     }
-    if (employee.rows.isDeleted) {
+    const employee = employeeResult.rows[0];
+    const employeeId = employee.id;
+
+    if (employee.isDeleted) {
       return res
         .status(403)
         .json({ error: "Employee is deleted you cannot check in/out" });
     }
-    const currentTime = new Date(); //getting current time
-    currentTime.setHours(0, 0, 0, 0); // setting time to 00:00:00 for date comparison
-    const attendance = await pool.query();
+    // const today = new Date(); //getting current time
+    // today.setHours(0, 0, 0, 0); // setting time to 00:00:00 for date comparison
+    const today = new Date().toLocaleDateString("en-CA");
+
+    // check for existing attendance
+    const existing = await pool.query(
+      `SELECT * FROM attendance 
+   WHERE employeeid = $1 AND date = $2 
+   LIMIT 1`,
+      [employeeId, today],
+    );
+
+    const now = new Date();
+
+    if (existing.rows.length === 0) {
+      // no existing attendance for today
+      const isLate =
+        now.getHours() > 9 || (now.getHours() === 9 && now.getMinutes() > 0);
+
+      const attendance = await pool.query(
+        `INSERT INTO attendance 
+     (employeeid, date, check_in, status) 
+     VALUES ($1, $2, $3, $4) 
+     RETURNING *`,
+        [employeeId, today, now, isLate ? "LATE" : "PRESENT"],
+      );
+
+      return res.status(200).json({
+        success: true,
+        type: "CHECK-IN",
+        data: attendance.rows[0],
+      });
+    } else if (existing.rows.length === 1 && !existing.rows[0].check_out) {
+      const checkInTime = new Date(existing.rows[0].check_in).getTime();
+      const now = new Date();
+
+      const diffMS = now.getTime() - checkInTime;
+      const diffHours = diffMS / (1000 * 60 * 60);
+
+      const workingHours = parseFloat(diffHours.toFixed(2));
+
+      let dayType = "Short Day";
+      if (workingHours >= 8) dayType = "Full Day";
+      else if (workingHours >= 6) dayType = "Three Quarter Day";
+      else if (workingHours >= 4) dayType = "Half Day";
+
+      //  Update DB properly
+      const updated = await pool.query(
+        `UPDATE attendance 
+     SET check_out = $1, workinghour = $2, daytype = $3 
+     WHERE id = $4 
+     RETURNING *`,
+        [now, workingHours, dayType, existing.rows[0].id],
+      );
+
+      return res.status(200).json({
+        success: true,
+        type: "CHECK-OUT",
+        data: updated.rows[0],
+      });
+    } else {
+      return res.status(400).json({
+        error: "Already checked out for today",
+      });
+    }
   } catch (error) {
+    console.log("Attendance error", error);
     res.status(500).json({ error: "Failed to check in/out" });
   }
 };
@@ -28,20 +95,23 @@ export const checkInOut = async (req, res) => {
 export const getAttendance = async (req, res) => {
   try {
     const userId = req.session.userId;
-    const employee = await pool.query(
+    const employeeResult = await pool.query(
       `SELECT * FROM employees WHERE userId=$1 LIMIT 1`,
       [userId],
     );
-    if (employee.rows.length === 0) {
+    if (employeeResult.rows.length === 0) {
       return res.status(404).json({ error: "Employee not found" });
     }
+    const employee = employeeResult.rows[0];
+    const employeeId = employee.id;
     const limit = req.query.limit || 30;
     const attendance = await pool.query(
-      `SELECT * FROM attendance WHERE userId=$1 ORDER BY date DESC LIMIT $2`,
-      [userId, limit],
+      `SELECT * FROM attendance WHERE employeeid=$1 ORDER BY date DESC LIMIT $2`,
+      [employeeId, limit],
     );
     res.status(200).json({ status: "success", data: attendance.rows });
   } catch (error) {
+    console.log("Failed to get attendance", error);
     res.status(500).json({ error: "Failed to get attendance" });
   }
 };
