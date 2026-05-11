7b90521a8ea988235222957c653822806c464eb8
koshish khadka <koshishkhadka364@gmail.com>
2026-05-11 17:16:52 +0545

    change local database to neon

--- server/config/db.js
+++ server/config/db.js
@@ -1,23 +1,49 @@
+// import { Pool } from "pg";
+// import dotenv from "dotenv";
+
+// dotenv.config();
+// const pool = new Pool({
+//   user: process.env.DB_USER,
+//   host: process.env.DB_HOST,
+//   database: process.env.DB_DATABASE,
+//   password: String(process.env.DB_PASSWORD),
+//   port: process.env.DB_PORT,
+// });
+
+// pool.connect((err, client, release) => {
+//   if (err) {
+//     return console.error("❌ Error acquiring client:", err.stack);
+//   }
+//   console.log("Database connection established successfully");
+//   release(); // Always release the client back to the pool!
+// });
+// export default pool;
 import { Pool } from "pg";
 import dotenv from "dotenv";
 
 dotenv.config();
+
+// const pool = new Pool({
+//   connectionString: process.env.DATABASE_URL,
+//   ssl: {
+//     rejectUnauthorized: false,
+//   },
+// });
 const pool = new Pool({
-  user: process.env.DB_USER,
-  host: process.env.DB_HOST,
-  database: process.env.DB_DATABASE,
-  password: String(process.env.DB_PASSWORD),
-  port: process.env.DB_PORT,
+  connectionString: process.env.DATABASE_URL,
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
 });
 
-// pool.on("connect", () => {
-//   console.log("Connected to the PostgreSQL database");
-// });
 pool.connect((err, client, release) => {
   if (err) {
-    return console.error("❌ Error acquiring client:", err.stack);
+    console.error("Error acquiring client:", err.stack);
+    return;
   }
-  console.log("Database connection established successfully");
-  release(); // Always release the client back to the pool!
+  console.log("Database connected successfully");
+  release();
 });
+
 export default pool;
